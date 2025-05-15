fbb70840a200de97063761964790d57cc9b81e2a
LeanFly <ningmuyu@live.com>
2025-05-15 09:34:07 +0800

    Update movies.py

--- movies.py
+++ movies.py
@@ -6,10 +6,13 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import yaml
 from easydict import EasyDict
-
+import curl_cffi
+import os
 
 def movieLinks(url="https://jable.tv/hot"):
-  with open("app.yaml", "r", encoding="utf-8") as f:
+  current_file = os.path.abspath(__file__)
+  current_dir = os.path.dirname(current_file)
+  with open(os.path.join(current_dir, "app.yaml"), "r", encoding="utf-8") as f:
     data = yaml.load(f, yaml.FullLoader)
     data = EasyDict(data)
     browser = data.browser
@@ -19,14 +22,17 @@ def movieLinks(url="https://jable.tv/hot"):
   
   links = []
   # dr = webdriver.Chrome()
-  with sync_playwright() as p:
-    browser = p.chromium.connect_over_cdp(browser) # ws://192.168.1.145:3333
+  # with sync_playwright() as p:
+    # browser = p.chromium.connect_over_cdp(browser) # ws://192.168.1.145:3333
 
-    page = browser.new_page()
+    # page = browser.new_page()
   # dr.get(url)
-    page.goto(type_page)
-    res = page.content()
-  bs = BeautifulSoup(res,"html.parser")
+    # page.goto(type_page)
+    # res = page.content()
+  
+  res = curl_cffi.get(url, impersonate="chrome131")
+  res.raise_for_status()
+  bs = BeautifulSoup(res.text,"html.parser")
   a_tags = bs.select('div.img-box>a')
 
   for a_tag in a_tags:
